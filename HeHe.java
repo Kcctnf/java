661bbdfdae0c2ea1c56233a0d091e5e096e2381a
Kcctnf <2647762039@qq.com>
2018-11-02 16:14:58 +0800

    提交

---
+++ HeHe.java
@@ -0,0 +1,59 @@
+// public class HeHe{
+	 // public static void main(String [] args){
+		 // //System.out.println("we will not use,Hello World!");
+		 // //print方法在输出之后不会换行。
+		 // System.out.print("hehe!");
+		 // System.out.print("hehe!");
+	 // }
+// }
+// import static java.lang.Math.*;
+// public class HeHe{
+	// public static void main(String [] args){
+		// double x = 4;
+		// double y = sqrt(x);
+		// //Math类中包含各种数学函数
+		// System.out.println(y);
+		// //利用关键字final指示常量，表示赋值之后无法再次更改
+		// //final double CM_PER_INCH = 2.54;
+		// //double paperWidth = 8.5;
+		// //double paperHeight = 11;
+		// //System.out.println("Paper size in centimeters: "+ paperWidth * CM_PER_INCH + "by" + 
+		 // //paperHeight * CM_PER_INCH);
+	// }
+// }
+// public class HeHe{
+	// public static final double CM_PER_INCH = 2.54;
+	// public static void main(String [] args){
+		 // static finfl 修饰一个类常量，在同一个类的其他方法中也可使用。
+		// 前边加上Public声明后，在其他类的方法中也可使用这个常量。
+		// double paperWidth = 8.5;
+		// double paperHeight = 11;
+		// System.out.println("Paper size in centimeters: "+ paperWidth * CM_PER_INCH + "by" + 
+		 // paperHeight * CM_PER_INCH);
+	// }
+// }
+public class HeHe{
+	public static void main(String [] args){
+		int num;
+		num = 100;
+		System.out.println("this is num:" + num);
+		num = num * 2;
+		System.out.print("this is num*2:");
+		System.out.println(num);
+	}
+	
+}
+public class IfSample{
+	public 
+}
+
+	
+
+
+
+	
+	
+	
+	
+	
+	
