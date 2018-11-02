08ae1acca457d330efee2efae7d3ca68765072de
Kcctnf <2647762039@qq.com>
2018-11-02 23:01:03 +0800

    提交

---
+++ day.11.02/ForTest.java
@@ -0,0 +1,55 @@
+// public class ForTest{
+	// public static void main(String [] args){
+		// int x;
+		// int y = 20;
+		// for(x = 0; x < 10; x = x + 1){
+			// System.out.println("This is x:" + x);
+			// System.out.println("This is y:" + y);
+			// y = y - 2;
+		// }
+	// }
+// }
+//面积
+public class ForTest{
+	public static void main(String [] args){
+		int x;
+		x = 10;
+		if(x == 10){
+			int y = 20;
+			System.out.println("y is:" + y);
+			x = x * 2;
+			System.out.println("x is:" + x);
+		}
+		
+		// int x;
+		// for(x = 0; x < 3; x++){
+			// int y = -1;System.out.println("y is:" + y);
+			// y = 100;
+			// System.out.println("y is:" + y);
+		// }
+		
+		// boolean b;
+		// b = false;
+		// System.out.println("b is " + b);
+		// b = true;
+		// System.out.println("b is " + b);
+		// //布尔变量的值本身就足以用来控制if语句。
+		// if(b)System.out.println("this is executed!");
+		// b = false;
+		// if(b)System.out.println("this is not executed!");
+		// System.out.println("10 > 9 is " + (10 > 9));
+		
+		// char ch1;
+		// char ch2;
+		// ch1 = 88;
+		// ch2 = 'Y';
+		// System.out.print("ch1和ch2:");
+		// System.out.println(ch1 + " " + ch2);
+		
+		// double pi , r , a;
+		// r = 10.8;
+		// pi = 3.1416;
+		// a = pi * r * r;
+		// System.out.println("面积为" + a);
+	}
+}
---
+++ day.11.02/IfSample.java
@@ -0,0 +1,13 @@
+public class IfSample{
+	public static void main(String args[]){
+		int x;
+		int y;
+		x = 10;
+		y = 20;
+		if(x < y)System.out.println("x is less than y");
+		x = x * 2;
+		if(x == y)System.out.println("x now equal to y");
+		x = x * 2;
+		if(x > y)System.out.println("x now greater than y");
+	}
+}
